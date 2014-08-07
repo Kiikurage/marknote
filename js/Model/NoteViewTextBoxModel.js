6fd3a073cba65d477b28bcc34aebd1c9d8274115
Kikura Yuichiro <y.kikura@gmail.com>
2014-08-07 22:02:42 +0900

    resizable

--- js/Model/NoteViewTextBoxModel.js
+++ js/Model/NoteViewTextBoxModel.js
@@ -4,11 +4,15 @@ var NoteViewTextboxModel = (function() {
 	function NoteViewTextboxModel() {
 		this._text = "";
 		this.__receiver = null;
+		this._w = 300;
+		this._z = 0;
 	}
 	extendClass(NoteViewTextboxModel, Model);
 
 	NoteViewTextboxModel.__record("x");
 	NoteViewTextboxModel.__record("y");
+	NoteViewTextboxModel.__record("z");
+	NoteViewTextboxModel.__record("w");
 	NoteViewTextboxModel.__record("text");
 	NoteViewTextboxModel.__record("focus");
 
